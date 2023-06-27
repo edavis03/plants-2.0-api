66e27b0aaf5f69636da3caf2a286e5d0acc06f8b
Emily Davis <demily@vmware.com>
2023-06-27 17:48:39 -0400

    Remove customer domain files generated by accelerator

--- src/main/resources/db/migration/V1.0__create_customer_profile_table.sql
+++ src/main/resources/db/migration/V1.0__create_customer_profile_table.sql
@@ -1,12 +1,3 @@
-CREATE TABLE customer_profile
-(
-    id         VARCHAR(36)  NOT NULL,
-    first_name VARCHAR(255) NULL,
-    last_name  VARCHAR(255) NULL,
-    email      VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
-);
-
 CREATE TABLE plant
 (
     id         UUID,
